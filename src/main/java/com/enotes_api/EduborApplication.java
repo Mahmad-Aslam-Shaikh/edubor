0ec048b8889a81907b8eaaa83da4e573c244fa7d
Aslam Shaikh <aslam.afzalpur759@gmail.com>
2024-11-19 20:09:15 +0530

    Initial commit

---
+++ src/main/java/com/enotes_api/EduborApplication.java
@@ -0,0 +1,13 @@
+package com.enotes_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduborApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EduborApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/enotes_api/EduborApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enotes_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduborApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
